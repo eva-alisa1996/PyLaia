21a3047e53df86db30442dd20e85d5208cca813c
Evaaa1996 <eva.koenders@medirisk.nl>
2025-04-16 05:45:29 +0200

    updata parameters

--- laia/data/transforms/vision/random_erasing.py
+++ laia/data/transforms/vision/random_erasing.py
@@ -4,7 +4,7 @@ import torch
 
 # random erasing with implemented torch erasing function
 def get_random_erasing(p=1):
-    return T.RandomErasing(p=p, scale=(0.1, 0.2), ratio=(5,15), value=0.5)
+    return T.RandomErasing(p=p, scale=(0.2, 0.5), ratio=(0.3,3.3), value=0.5)
 
 # data wrapper
 class AugmentedImageTransform(torch.nn.Module):
